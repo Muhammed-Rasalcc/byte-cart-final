f257bf42c767d8528b0ac9af3f028babf760d526
Muhammed-Rasalcc <rasalcc9@gmail.com>
2025-04-23 21:32:14 +0530

    first commit

--- src/redux/features/orders/orderApi.js
+++ src/redux/features/orders/orderApi.js
@@ -1,5 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { getBaseUrl } from '../../../utils/baseUrl';
+import { getBaseUrl } from 'utils/baseURL';
+
 
 
 const orderApi = createApi({
