74b42af3f19bb176dbb8b6cf0cd9c1585d40b5b9
Muhammed-Rasalcc <rasalcc9@gmail.com>
2025-04-23 20:35:41 +0530

    first commit

--- src/redux/features/auth/authApi.js
+++ src/redux/features/auth/authApi.js
@@ -1,5 +1,5 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
-import { getBaseUrl } from '../../../utils/baseUrl'
+import { getBaseUrl } from '../../../utils/baseURL'
 import { logout } from './authSlice';
 
 const authApi = createApi({
